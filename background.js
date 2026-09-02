828a5e7eadbefc1d5c376a50b57029cea74fa3ac
Daiss216 <shimulsuplekar21@gmail.com>
2026-09-02 17:55:08 +0530

    Saving and retrieving data in the background

--- background.js
+++ background.js
@@ -1,1 +1,32 @@
+// let data= {                    //objrct
+//     "event": 'onStop/onStart',
+//     "prefs": {                 //val in popup.html
+//         "locationId": '123',
+//         "startDate": "2026-09-02",
+//         "endDate": "2026-10-02"
+//     }
+// }
 
+chrome.runtime.onMessage.addListener((data) => {
+  const { event, prefs } = data; //destructuring
+  switch (event) {
+    case "onStop":
+      handleOnStop();
+      break;
+    case "onStart":
+      handleOnStart(prefs);
+      break;
+    default:
+      break;
+  }
+});
+
+const handleOnStop = () => {
+  console.log("On Stop in background");
+};
+
+const handleOnStart = (prefs) => {
+  console.log("On Start in background");
+  console.log("prefs received: ", prefs);
+  chrome.storage.local.set(prefs); //storing the preferences throgh storage chrome api
+};
--- popup/popup.js
+++ popup/popup.js
@@ -7,14 +7,30 @@ const endDateElement = document.getElementById("enddate");
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
 
-startButton.onclick = function () {
-  if (startDateElement.value) {
-    console.log("Start Date Element:", startDateElement.value);
-  } else {
-    console.log("Start Date is Invalid!");
-  }
+startButton.onclick = () => {
+  const prefs = {
+    locationId: locationIdElement.value,
+    startdate: startDateElement.value,
+    enddate: endDateElement.value,
+  };
+  chrome.runtime.sendMessage({ event: "onStart", prefs });
 };
 
-stopButton.onclick = function () {
-  console.log("End date:", endDateElement.value);
+stopButton.onclick = () => {
+  chrome.runtime.sendMessage({ event: "onStop" });
 };
+
+//chrome storage API
+chrome.storage.local.get(["locationId", "startdate", "enddate"], (result) => {
+  const { locationId, startdate, enddate } = result;
+
+  if (locationId) {
+    locationIdElement.value = locationId;
+  }
+  if (startdate) {
+    startDateElement.value = startdate;
+  }
+  if (enddate) {
+    endDateElement.value = enddate;
+  }
+});
