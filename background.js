66353214519e909349ecdc46b8ca5b4db8bc0f33
Daiss216 <shimulsuplekar21@gmail.com>
2026-09-06 15:40:47 +0530

    implemented job scheduler with Alarm API

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 import fetchLocation from "./api/fetchLocaton.js";
+const ALARM_NAME = "ALARM";
 
 chrome.runtime.onInstalled.addListener((detailes) => {
   fetchLocation();
@@ -20,10 +21,28 @@ chrome.runtime.onMessage.addListener((data) => {
 
 const handleOnStop = () => {
   console.log("On Stop in background");
+  stopAlarm();
 };
 
 const handleOnStart = (prefs) => {
-  console.log("On Start in background");
   console.log("prefs received: ", prefs);
   chrome.storage.local.set(prefs); //storing the preferences throgh storage chrome api
+  createAlarm();
 };
+
+const createAlarm = () => {
+  chrome.alarms.get(ALARM_NAME, (existingAlarm) => {
+    //to avoid overlapping
+    if (!existingAlarm) {
+      chrome.alarms.create(ALARM_NAME, { periodInMinutes: 1.0 });
+    }
+  });
+};
+
+const stopAlarm = () => {
+  chrome.alarms.clearAll();
+};
+
+chrome.alarms.onAlarm.addListener(() => {
+  console.log("OnAlarm scheduled code runnning...");
+});
